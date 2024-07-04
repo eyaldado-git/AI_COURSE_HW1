8d6da53ab69d018e808adc1f24030d0850ff380e
eyal dado <eyaldado2@gmail.com>
2024-07-04 07:45:23 +0300

    cosmetic changes

--- Algorithms.py
+++ Algorithms.py
@@ -5,7 +5,17 @@ import heapdict
 from CampusEnv import CampusEnv
 
 class Node:
-    def __init__(self, state=0, parent=None, cost=0, heuristic = 0, h_weight = 0 ):
+    """
+    Initializes a new node.
+
+    Args:
+        state (int): The state represented by this node.
+        parent (Node, optional): The parent node. Defaults to None.
+        cost (float): The cost to reach this node. Defaults to 0.0.
+        heuristic (int): The heuristic estimate from this node to the goal. Defaults to 0.
+        h_weight (float): The weight of the heuristic in cost calculation. Defaults to 0.0.
+    """
+    def __init__(self, state: int = 0, parent = None, cost: float = 0.0 , heuristic: int = 0, h_weight: float = 0 ) -> None:
         self.state = state
         self.parent = parent
         self.cost = cost
@@ -13,15 +23,24 @@ class Node:
         self.h_weight = h_weight
 
     @staticmethod
-    def make_node(state: int, parent=None, cost: float = 0, heuristic = 0,  h_weight = 0 ):
+    def make_node(state: int, parent = None, cost: float = 0, heuristic: int = 0,  h_weight: float = 0 ):
+        """
+        Static method to create a new node.
+
+        Args:
+            state (int): The state represented by this node.
+            parent (Node, optional): The parent node. Defaults to None.
+            cost (float): The cost to reach this node. Defaults to 0.0.
+            heuristic (int): The heuristic estimate from this node to the goal. Defaults to 0.
+            h_weight (float): The weight of the heuristic in cost calculation. Defaults to 0.0.
+
+        Returns:
+            Node: A new instance of Node.
+        """
         return Node(state, parent, cost, heuristic, h_weight)
     
-    def __eq__(self, other):
-        return isinstance(other, Node) and self.state == other.state
-    
     def __repr__(self) -> str:
-        return f"Node({self.state},  {(1 - self.h_weight) * self.cost + self.h_weight * self.heuristic:.2f})"
-        # return f"Node(state={self.state}, parent={self.parent}, cost={self.cost})"
+        return f"Node(state={self.state}, parent={self.parent}, cost={self.cost})"
     
     def __str__(self) -> str:
         return f"Node({self.state}, {(1 - self.h_weight) * self.cost + self.h_weight * self.heuristic:.2f})"
@@ -29,44 +48,88 @@ class Node:
     def __eq__(self, other):
         if not isinstance(other, Node):
             return NotImplemented
-        return self.state == other.state and self.cost == other.cost
+        return self.state == other.state and self.cost == other.cost and self.parent == other.parent
 
     def __lt__(self, other):
         if not isinstance(other, Node):
             return NotImplemented
-        if (1 - self.h_weight) * self.cost + self.h_weight * self.heuristic == (1 - self.h_weight) * other.cost + self.h_weight * other.heuristic:
-            return self.state < other.state
-        return (1 - self.h_weight) * self.cost + self.h_weight * self.heuristic < (1 - self.h_weight) * other.cost + self.h_weight * other.heuristic
-
+        current_f = (1 - self.h_weight) * self.cost + self.h_weight * self.heuristic
+        other_f = (1 - self.h_weight) * other.cost + self.h_weight * other.heuristic
+        return (current_f, self.state) < (other_f, other.state)
 class Utility:
     @staticmethod
-    def action_between_nodes(child: Node, parent: Node) -> int:
-        if parent.state - child.state == -1:
-            return 1  # Move right
-        elif parent.state - child.state == 1:
-            return 3  # Move left
-        elif parent.state - child.state < -1:
+    def action_between_nodes(*, child: Node, parent: Node) -> int:
+        """
+        Determine the action taken to move from the parent node to the child node.
+
+        Args:
+            child (Node): The child node.
+            parent (Node): The parent node.
+
+        Returns:
+            int: An integer representing the action:
+                - 0 for moving down
+                - 1 for moving right
+                - 2 for moving up
+                - 3 for moving left
+        """
+        if parent.state - child.state < -1:
             return 0  # Move down
+        elif parent.state - child.state == -1:
+            return 1  # Move right
         elif parent.state - child.state > 1:
             return 2  # Move up
+        else:
+            return 3  # Move left
 
     @staticmethod
     def solution(node: Node, expended: int) -> Tuple[List[int], float, int]:
+        """
+        Reconstruct the solution path from a node to the root node and calculate the total cost.
+
+        Args:
+            node (Node): The end node of the solution path.
+            expended (int): The number of nodes expanded during the search.
+
+        Returns:
+            Tuple[List[int], float, int]: A tuple containing:
+                - A list of actions to reach the solution.
+                - The total cost of the solution path.
+                - The number of nodes expanded.
+        """
         actions = []
         cost = 0.0
         while node.parent is not None:
-            actions.append(Utility.action_between_nodes(node, node.parent))
+            actions.append(Utility.action_between_nodes(child=node, parent=node.parent))
             cost += node.cost
             node = node.parent
         actions.reverse()  # Reverse actions to get them in the correct order
         return actions, cost, expended
 
 class CampusHeuristic:
+    """
+     A heuristic for a campus environment.
+
+    The heuristic value is calculated as the minimum of the Manhattan distance to any goal state and the portal cost.
+
+    Args:
+        env (CampusEnv): The environment representing the campus.
+        portal_cost (float): The cost to use a portal.
+    """
     def __init__(self, env: CampusEnv, portal_cost: float):
         self.env = env
         self.portal_cost = portal_cost
 
     def get_heuristic_value(self, state: int) -> float:
+        """
+        Calculate the heuristic value for a given state.
+
+        Args:
+            state (int): The current state for which the heuristic value is calculated.
+
+        Returns:
+            float: The heuristic value
+        """
         possible_heuristic_values = [self.portal_cost]
         state_row, state_col = self.env.to_row_col(state)
         for goal_state in self.env.get_goal_states():
@@ -86,13 +149,13 @@ class DFSGAgent(Agent):
     def __init__(self):
         super().__init__()
 
-    def search(self, env) -> Tuple[List[int], float, int]:
+    def search(self, env: CampusEnv) -> Tuple[List[int], float, int]:
         init_node = Node.make_node(env.get_initial_state())
         open_list = [init_node]
         close_list = []
         return self.recursiveDFSG(env, open_list, close_list)
     
-    def recursiveDFSG(self, env, open_list, close_list) -> Tuple[List[int], float, int]: 
+    def recursiveDFSG(self, env: CampusEnv, open_list: List[Node], close_list: List[int]) -> Tuple[List[int], float, int]: 
         node = open_list.pop()
         close_list.append(node.state)
 
@@ -117,7 +180,7 @@ class UCSAgent(Agent):
         self.open_list = heapdict.heapdict() 
         self.close_list = []
     
-    def search(self, env) -> Tuple[List[int], float, int]:
+    def search(self, env: CampusEnv) -> Tuple[List[int], float, int]:
         init_node = Node.make_node(env.get_initial_state())
         self.open_list[init_node.state] = init_node
         self.close_list = []
@@ -197,8 +260,8 @@ if __name__ == "__main__":
             "HFFG"],
     "4x4_2": ["SFFF",
             "FFFF",
-            "FFFH",
-            "FFHG"], # 0->4->8->12->13->14
+            "FFFF",
+            "FFFG"], # 0->4->8->12->13->14
     "8x8": ["SFFFFFFF",
             "FFFFFTAL",
             "TFFHFFTF",
